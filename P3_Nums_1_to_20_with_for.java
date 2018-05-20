f579a146b913e717e14c9d40e5b0f492942637f1
bdbelevski <34931754+bdbelevski@users.noreply.github.com>
2018-05-20 17:31:55 +0300

    Add files via upload

---
+++ P3_Nums_1_to_20_with_for.java
@@ -0,0 +1,7 @@
+public class P3_Nums_1_to_20_with_for {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 20; i++) {
+            System.out.println(i);
+        }
+    }
+}
